5ab651b5db9d7cfe6b200fcd8b8f8d16547fec0c
Andrey Shalashov <avshalashov@yandex.ru>
2024-02-11 20:54:07 +0300

    remove unused http handlers

--- internal/adapter/http/contract.go
+++ internal/adapter/http/contract.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/k1nky/gophkeeper/internal/entity/user"
-	"github.com/k1nky/gophkeeper/internal/entity/vault"
 )
 
 //go:generate mockgen -source=contract.go -destination=mock/auth.go -package=mock authService
@@ -12,13 +11,6 @@ type authService interface {
 	Register(ctx context.Context, u user.User) (string, error)
 	Login(ctx context.Context, u user.User) (string, error)
 	Authorize(token string) (user.PrivateClaims, error)
-}
-
-type keeperService interface {
-	PutSecret(ctx context.Context, meta vault.Meta, data *vault.DataReader) (*vault.Meta, error)
-	GetSecretData(ctx context.Context, uk vault.MetaID) (*vault.DataReader, error)
-	GetSecretMeta(ctx context.Context, uk vault.MetaID) (*vault.Meta, error)
-	ListSecretsByUser(ctx context.Context) (vault.List, error)
 }
 
 type logger interface {
